32808ca84d740d4d28a0bf9794c2731cb2fb219e
Pernilla Göth <pg222jx@student.lnu.se>
2021-01-14 14:24:38 +0100

    Changing about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,23 +10,17 @@ class Blog extends Component {
             <div className="container showcase-inner">
               <div className="about">
               <h1>About me</h1>
-
-              <p>My name is Pernilla Göth and I am currently studying my second year<img src={profilePic} alt="Pernilla Göth" className="profilePic" ALIGN="right"/>
+              <img src={profilePic} alt="Pernilla Göth" className="profilePic"/>
+              <p>My name is Pernilla Göth and I am currently studying my second year
               <br/>to become a fullstack web developer at Linneaus Univeristy.
-              <br/>When I am not sitting in front of a computer perfecting my programming skills
-              I usually have a cup of tea in one hand and a book in the other.
-              You can also find me with a paintbrush, camera or in front of a sewing machine. But my favorite thing
-              is to sing, with friends or alone with my guitar.
-              <br/>You could say I like to be creative.
+              <br/>I love to be creative. Painting, photography and music are three of my favorite things to do in my free time.
+
               <br/>
-              <br/>Because of my creative personality I love to work with frontend development. 
-              I love when things look good and I love to be the one making them so.
-              <br/>But what I really enjoy about web development is the puzzle - the constant problem solving
-              <br/>and refactoring to make the code more readable and understandable. Clean code is great code!
-              <br/>But I also really like backend! I believe that it is always important to keep code readable
-              <br/>and understandable, but maybe even more so on the server side!
+              I am a brainstormer and love discussing ideas and finding solutions.<br/>
+              Yes, I will be fresh out of university in june and my experience in working with programming is limited. But I am eager to learn and have been told to be  a fast learner.
               <br/><br/>If you want to contact me or learn more about me don't hesitate to check out my <a href="https://www.linkedin.com/in/pernilla-g%C3%B6th-69027319b/" target="_blanc">LinkedIn page!</a>
               </p>
+              <a href="https://docs.google.com/document/d/16SugJKIyc6cJPo5Wt9CwZe1q5FzU_559Xre8Td7LnCQ/edit#heading=h.61e3cm1p1fln" target="_blanc" className="btn">Resume</a>
               </div>
             </div>
           </header>
