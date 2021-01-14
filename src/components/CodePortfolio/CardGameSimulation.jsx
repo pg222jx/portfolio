17e05be2bac5aa30a2e7da174e1421d2bc57580d
Pernilla Göth <pg222jx@student.lnu.se>
2021-01-14 12:19:32 +0100

    Trying to find building bug

--- src/components/CodePortfolio/CardGameSimulation.jsx
+++ src/components/CodePortfolio/CardGameSimulation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import cardGamePic from '../../images/CardGame.JPG'
+// import cardGamePic from '../../images/CardGame.JPG'
 
 class CardGameSimulation extends Component {
     state = {  }
@@ -8,7 +8,7 @@ class CardGameSimulation extends Component {
         return (
             <div className="codePortfolio">
               <h2>21 card game simulator</h2>
-              <img src={cardGamePic} alt="Pernilla Göth" className="codePics"/>
+              <img src="C:/Users/perni/Portfolio/portfolio/src/images/CardGame.JPG" alt="Pernilla Göth" className="codePics"/>
               <p>As the title suggest this is a simulatior for the card game 21.<br/>
               It was created to play with object orientation using C#. </p><br/>
               <p>C#, .Net</p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import cardGamePic from '../images/CardGame.JPG'
 
 class Header extends Component {
     state = {  }
@@ -9,7 +8,6 @@ class Header extends Component {
         return (
             <header className="showcase">
             <div className="container showcase-inner">
-            <img src={cardGamePic} alt="Pernilla Göth" className="codePics"/>
               <h1>- Pernilla Göth - </h1>
               <p>Combining a passion for creativity with a degree in fullstack web development</p> 
               <Link to="/code" className="btn">Personal projects</Link>
