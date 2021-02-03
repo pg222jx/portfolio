5f1fcb10b85a4f9714e9cc15d1b1e1908890e18d
Pernilla Göth <pg222jx@student.lnu.se>
2021-02-03 21:30:45 +0100

    Changing CSS for contact moduel

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,11 @@ class Contact extends Component {
             // <header className="showcase">
             <div className="container showcase-inner">
               <h1>Contact</h1>
-              <p>Email: <a href="mailto:pernilla.goeth@gmail.com">pernilla.goeth@gmail.com</a></p>
-              <p>LinkedIn: <a href="https://www.linkedin.com/in/pernilla-g%C3%B6th/">Profile</a></p>
+              <div className="contact">
+              <p>EMAIL: <a href="mailto:pernilla.goeth@gmail.com"> pernilla.goeth@gmail.com</a></p>
+              <p>LINKEDIN: <a href="https://www.linkedin.com/in/pernilla-g%C3%B6th/">Profile</a></p>
+              <p>PHONE: 0762985381</p>
+              </div>
             </div>
           // </header>
         )
