5c1860dc5b288ee9e18d7948ceddb11d421a77e9
Pernilla Göth <pg222jx@student.lnu.se>
2021-02-04 15:56:53 +0100

    Adding info to resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,11 +5,9 @@ class Resume extends Component {
 
     render() { 
         return (
-            // <header className="resume">
             <div className="container showcase-inner">
-              
                 <div className="resume">
-                <h1>Resume</h1>
+                <h1 className="resumeTitle">Resume</h1>
               <h2>Languages</h2>
               <div className="row">
                 <div className="column left">
@@ -45,19 +43,58 @@ class Resume extends Component {
               </div>
 
               <h2>Education</h2>
+              <p>2019-2021</p>
               <div className="row">
-                <div className="column left"></div>
-                <div className="column right"></div>
+                <div className="column left">
+                  <h3>Web Developer</h3>
+                  <h4>Linnaeus University</h4>
+                </div>
+                <div className="column right">
+                <p>Just started my last term  to become  a Web Developer. 
+                  I have studied software quality, testing,  analysis and design together with learning different languages and frameworks.
+                  The last term focuses on  group projects, software architectures and Internet of Things.</p>
+                </div>
+              </div>
+
+              <p>2013-2014</p>
+              <div className="row">
+                <div className="column left">
+                  <h3>Literary Studies</h3>
+                  <h4>Gothenburg University</h4>
+                </div>
+                <div className="column right">
+                <p>What can literature tell us about people's thoughts and conditions throughout history? Why do certain texts live on, and how do you really determine what is good literature?
+                We immersed ourselves in literary classics and at the same time problematized the concept of classics and questions about canon and literary history writing.
+                </p>
+                </div>
               </div>
 
               <h2>Employment</h2>
+              <p>2017-PRESENT</p>
               <div className="row">
-                <div className="column left"></div>
-                <div className="column right"></div>
+                <div className="column left">
+                  <h3>Optician assistant</h3>
+                  <h4>Synsam</h4>
+                </div>
+                <div className="column right">
+                  <p>To be good in this job you need to have skills in customer service as well as have a huge understanding of the technical aspects of optics. 
+                    You need to take this knowledge and present it to customers with patience.
+                    I made decisions together with the opticians every day to make sure that how the optic should work and how the customer feels are both equally measured. </p>
+                </div>
+              </div>
+              
+              <p>2011-2017</p>
+              <div className="row">
+                <div className="column left">
+                  <h3>Seller, Barista</h3>
+                  <h4>Jeans Company, Espresso House & Nöjds Confectionery</h4>
+                </div>
+                <div className="column right">
+                <p>Stress resistance, customer service and always a smile on my face.</p>
+                </div>
               </div>
               </div>
             </div>
-          // </header>
         )
     }
 }
